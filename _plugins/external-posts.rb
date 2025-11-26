4c80e521c8b435adfcf5fdc5b956b9431f95f475
rudresh-v <143840278+rudresh-v@users.noreply.github.com>
2025-11-26 06:06:05 +0000

    bugfix

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -10,23 +10,35 @@ module ExternalPosts
     def generate(site)
       if site.config['external_sources'] != nil
         site.config['external_sources'].each do |src|
-          p "Fetching external posts from #{src['name']}:"
-          xml = HTTParty.get(src['rss_url']).body
-          feed = Feedjira.parse(xml)
-          feed.entries.each do |e|
-            p "...fetching #{e.url}"
-            slug = e.title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-            path = site.in_source_dir("_posts/#{slug}.md")
-            doc = Jekyll::Document.new(
-              path, { :site => site, :collection => site.collections['posts'] }
-            )
-            doc.data['external_source'] = src['name'];
-            doc.data['feed_content'] = e.content;
-            doc.data['title'] = "#{e.title}";
-            doc.data['description'] = e.summary;
-            doc.data['date'] = e.published;
-            doc.data['redirect'] = e.url;
-            site.collections['posts'].docs << doc
+          begin
+            p "Fetching external posts from #{src['name']}:"
+            xml = HTTParty.get(src['rss_url']).body
+            feed = Feedjira.parse(xml)
+            
+            # Check if feed was successfully parsed
+            if feed == false || feed.nil?
+              p "Warning: Could not parse feed from #{src['name']}"
+              next
+            end
+            
+            feed.entries.each do |e|
+              p "...fetching #{e.url}"
+              slug = e.title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+              path = site.in_source_dir("_posts/#{slug}.md")
+              doc = Jekyll::Document.new(
+                path, { :site => site, :collection => site.collections['posts'] }
+              )
+              doc.data['external_source'] = src['name'];
+              doc.data['feed_content'] = e.content;
+              doc.data['title'] = "#{e.title}";
+              doc.data['description'] = e.summary;
+              doc.data['date'] = e.published;
+              doc.data['redirect'] = e.url;
+              site.collections['posts'].docs << doc
+            end
+          rescue => e
+            p "Error fetching external posts from #{src['name']}: #{e.message}"
+            next
           end
         end
       end
